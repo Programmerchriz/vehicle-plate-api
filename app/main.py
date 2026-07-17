c7347fcddc8c69e3677e322fb7b44384c330a43d
ProgrammerChris6002 <ProgrammerChris6002@gmail.com>
2026-07-17 18:04:45 +0100

    - Added vehicle route to main.py - Removed pgadmin from docker compose file

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.config.settings import settings
 from app.routers.auth import router as auth_router
-from app.routers.test import router as test_router
+from app.routers.vehicle import router as vehicle_router
+# from app.routers.test import router as test_router
 
 app = FastAPI(
   title=settings.APP_NAME,
@@ -23,7 +24,8 @@ app.add_middleware(
 )
 
 app.include_router(auth_router)
-app.include_router(test_router)
+app.include_router(vehicle_router)
+# app.include_router(test_router)
 
 
 @app.get("/")
